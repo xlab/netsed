1afdcab9622d5248a87972b474b04f79bca00483
Julien Viard de Galbert <julien@vdg.blogsite.org>
2010-09-13 16:16:43 +0200

    License clarification and changes.
    Added GPL v2+ in files, added optional Ruby license to ruby files also.

--- netsed.c
+++ netsed.c
@@ -1,7 +1,31 @@
-//  This work is based on the original netsed 0.01c by Michal Zalewski.
-//  Please contact Julien VdG <julien@silicone.homelinux.org> if you encounter
-//  any problems with this version.
-//  The changes compared to version 0.01c are related in the NEWS file.
+/*
+  netsed 1.00b          (C) 2010  Julien VdG <julien@silicone.homelinux.org>
+  --------------------------------------------------------------------------
+
+  This work is based on the original netsed:
+      netsed 0.01c      (C) 2002  Michal Zalewski <lcamtuf@ids.pl>
+
+  Please contact Julien VdG <julien@silicone.homelinux.org> if you encounter
+  any problems with this version.
+  The changes compared to version 0.01c are related in the NEWS file.
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+
 
 ///@mainpage
 ///
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,27 @@
 #!/usr/bin/ruby
 # helper function for netsed Unit::Tests
 # (c) 2010 Julien Viard de Galbert <julien@silicone.homelinux.org>
-# 
+#
+# ---------------------------------------------------------------------------
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# You may also redistribute it or any part of it under the Ruby license to
+# better integrate to your ruby scripts.
+# The ruby license is available at http://www.ruby-lang.org/en/LICENSE.txt
+# ---------------------------------------------------------------------------
+
 
 require 'socket'
 
