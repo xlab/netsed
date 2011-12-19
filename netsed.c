b93c11659598ac6a9cc6b7436ec672fb44b390b0
Mats Erik Andersson <debian@gisladisker.se>
2011-12-19 17:26:04 +0100

    Fix for option IPV6_V6ONLY.
    Without the new test, the listening socket is single stacked since the
    computation used "af" instead of "family".

--- netsed.c
+++ netsed.c
@@ -531,7 +531,7 @@ void bind_and_listen(int af, int tcp, const char *portstr) {
     setsockopt(lsock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
     //fcntl(lsock,F_SETFL,O_NONBLOCK);
     /* Make our best to decide on dual-stacked listener. */
-    one = (af == 0) ? 0 /* AF_UNSPEC given */ : 1; /* Preconditioned addr */
+    one = (family == AF_UNSPEC) ? 0 /* All families */ : 1; /* Preconditioned addr */
     if (res->ai_family == AF_INET6)
       if (setsockopt(lsock, IPPROTO_IPV6, IPV6_V6ONLY, &one, sizeof(one)))
         printf("    Failed to unset IPV6_V6ONLY: %s.\n", strerror(errno));
