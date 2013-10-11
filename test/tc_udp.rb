a668c945f46275efa99ba5d4df88fc246e5cbaf7
Julien Viard de Galbert <julien@vdg.blogsite.org>
2013-10-11 20:47:25 +0200

    Fix ruby doc generation.
    Removing UTF8 unbreakable space from ruby file fixed it!

--- test/tc_udp.rb
+++ test/tc_udp.rb
@@ -7,12 +7,12 @@
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# of the License, or (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
@@ -150,6 +150,6 @@ end
 # inspired by http://www.ruby-forum.com/topic/204730.
 #TC_UDPTest6=Class.new(TC_UDPTest)
 #TC_UDPTest6.const_set(:SERVER, LH_IPv6)
-# for some reasons UDPSocket.connect fails with IPv6 addr :(
+# for some reasons UDPSocket.connect fails with IPv6 addr :(
 
 # vim:sw=2:sta:et:
